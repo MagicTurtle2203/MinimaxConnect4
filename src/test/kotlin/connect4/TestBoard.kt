e53549de24755eeda8eaa08521ab91954caad513
Rafferty Chen <raffertychen@gmail.com>
2020-07-17 06:05:48 -0700

    Add tests for horizontal + vertical winner checks

--- src/test/kotlin/connect4/TestBoard.kt
+++ src/test/kotlin/connect4/TestBoard.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.*
 class TestBoard {
     @Test
     fun testBoardIsCreatedWithCorrectDimensions() {
-        val b = Board(5, 10)
+        val b = Board(10, 5)
         Assertions.assertEquals(10, b.board.size)
         for (element in b.board) {
             Assertions.assertEquals(5, element.size)
@@ -33,7 +33,7 @@ class TestBoard {
 
     @Test
     fun testDropHandlesMultipleDropsOnSameColumnCorrectly() {
-        val b = Board(5, 1)
+        val b = Board(1, 5)
         for (i in 0 until 5) {
             b.drop(0)
         }
@@ -42,16 +42,295 @@ class TestBoard {
 
     @Test
     fun testDropThrowsExceptionForInvalidColumnNumber() {
-        val b = Board(5, 1)
+        val b = Board(1, 5)
         Assertions.assertThrows(InvalidMoveException::class.java) { b.drop(1) }
     }
 
     @Test
     fun testDropThrowsExceptionIfColumnFull() {
-        val b = Board(5, 1)
+        val b = Board(1, 5)
         for (i in 0 until 5) {
             b.drop(0)
         }
         Assertions.assertThrows(InvalidMoveException::class.java) { b.drop(0) }
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalX1() {
+        val b = Board(7, 6)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertEquals(Pair(true, 'X'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalX2() {
+        val b = Board(7, 6)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertEquals(Pair(true, 'X'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalX3() {
+        val b = Board(7, 6)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertEquals(Pair(true, 'X'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalX4() {
+        val b = Board(7, 6)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(1)
+        Assertions.assertEquals(Pair(true, 'X'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalY1() {
+        val b = Board(7, 6)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertEquals(Pair(true, 'Y'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalY2() {
+        val b = Board(7, 6)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertEquals(Pair(true, 'Y'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalY3() {
+        val b = Board(7, 6)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertEquals(Pair(true, 'Y'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerHorizontalY4() {
+        val b = Board(7, 6)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(4)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(1)
+        Assertions.assertEquals(Pair(true, 'Y'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerVerticalX1() {
+        val b = Board(7, 6)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertEquals(Pair(true, 'X'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerVerticalX2() {
+        val b = Board(7, 6)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertEquals(Pair(true, 'X'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerVerticalX3() {
+        val b = Board(7, 6)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertEquals(Pair(true, 'X'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerVerticalY1() {
+        val b = Board(7, 6)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(1)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertEquals(Pair(true, 'Y'), b.checkWinner())
+    }
+    @Test
+    fun testBoardFindsWinnerVerticalY2() {
+        val b = Board(7, 6)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(3)
+        Assertions.assertEquals(Pair(true, 'Y'), b.checkWinner())
+    }
+
+    @Test
+    fun testBoardFindsWinnerVerticalY3() {
+        val b = Board(7, 6)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(0)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(2)
+        Assertions.assertFalse(b.checkWinner().first)
+        b.drop(6)
+        Assertions.assertEquals(Pair(true, 'Y'), b.checkWinner())
     }
 }
