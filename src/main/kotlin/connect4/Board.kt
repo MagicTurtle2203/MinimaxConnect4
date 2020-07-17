19eee160ba8672773ec6cc1fa2173d7b06b92019
Rafferty Chen <raffertychen@gmail.com>
2020-07-16 22:20:01 -0700

    Fix getting index in drop

--- src/main/kotlin/connect4/Board.kt
+++ src/main/kotlin/connect4/Board.kt
@@ -1,27 +1,24 @@
 package connect4
 
 class Board(private val numRows: Int, private val numCols: Int) {
-    enum class Players { X, Y }
+    private enum class Players { X, Y }
+    private var turn: Players = Players.X
     val board: Array<CharArray> = Array(numCols) { CharArray(numRows) { ' ' } }
-    var turn: Players = Players.X
 
     fun drop(colNumber: Int) {
         if (colNumber < 0 || colNumber >= numCols) {
             throw InvalidMoveException("Specified column number is not valid")
         }
 
-        for ((index, value) in board[colNumber].withIndex()) {
-            if (value != ' ') {
-                board[colNumber][index - 1] = when (turn) {
-                    Players.X -> {
-                        turn = Players.Y
-                        'X'
-                    }
-                    Players.Y -> {
-                        turn = Players.X
-                        'Y'
-                    }
-                }
+        val index = board[colNumber].lastIndexOf(' ')
+        board[colNumber][index] = when (turn) {
+            Players.X -> {
+                turn = Players.Y
+                'X'
+            }
+            Players.Y -> {
+                turn = Players.X
+                'Y'
             }
         }
     }
