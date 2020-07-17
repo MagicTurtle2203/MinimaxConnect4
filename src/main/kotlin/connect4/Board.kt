2829deb8ae26f3bfb2d47d507e5b415a562de174
Rafferty Chen <raffertychen@gmail.com>
2020-07-16 22:03:29 -0700

    Add drop function to Board

--- src/main/kotlin/connect4/Board.kt
+++ src/main/kotlin/connect4/Board.kt
@@ -1,9 +1,28 @@
 package connect4
 
 class Board(private val numRows: Int, private val numCols: Int) {
-    private enum class Players {
-        X, Y
+    enum class Players { X, Y }
+    val board: Array<CharArray> = Array(numCols) { CharArray(numRows) { ' ' } }
+    var turn: Players = Players.X
+
+    fun drop(colNumber: Int) {
+        if (colNumber < 0 || colNumber >= numCols) {
+            throw InvalidMoveException("Specified column number is not valid")
+        }
+
+        for ((index, value) in board[colNumber].withIndex()) {
+            if (value != ' ') {
+                board[colNumber][index - 1] = when (turn) {
+                    Players.X -> {
+                        turn = Players.Y
+                        'X'
+                    }
+                    Players.Y -> {
+                        turn = Players.X
+                        'Y'
+                    }
+                }
+            }
+        }
     }
-    private val board: Array<CharArray> = Array(numCols) { CharArray(numRows) { ' ' } }
-    private var turn: Players = Players.X
 }
