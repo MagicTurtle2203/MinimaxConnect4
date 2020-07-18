a5c817b092c8845aa354e797c73e88dab1c18651
Rafferty Chen <raffertychen@gmail.com>
2020-07-18 14:18:45 -0700

    Complete PlayerAgent class

--- src/main/kotlin/connect4/PlayerAgent.kt
+++ src/main/kotlin/connect4/PlayerAgent.kt
@@ -1,10 +1,51 @@
 package connect4
 
+import java.lang.NumberFormatException
 import kotlin.io.readLine
 
 class PlayerAgent(numCols: Int, numRows: Int, lengthToWin: Int, popOut: Boolean)
     : AI(numCols, numRows, lengthToWin, popOut) {
     override fun getMove(): Move {
-        TODO("Not yet implemented")
+        val moveType: MoveType
+        val columnNumber: Int
+
+        inputmt@ while (true) {
+            print("Enter move type: ")
+            val inputMoveType = readLine()
+            moveType = when (inputMoveType!!.toLowerCase()) {
+                "drop" -> MoveType.DROP
+                "pop" -> {
+                    if (!popOut) {
+                        println("Popping is not allowed in this game mode")
+                        continue@inputmt
+                    }
+                    MoveType.POP
+                }
+                else -> {
+                    println("Invalid move type entered")
+                    continue@inputmt
+                }
+            }
+            break
+        }
+
+        inputcn@ while (true) {
+            print("Enter column number: ")
+            val inputColumnNumber: Int
+            try {
+                inputColumnNumber = readLine()!!.toInt()
+            } catch (e: NumberFormatException) {
+                println("Invalid number")
+                continue@inputcn
+            }
+            if (inputColumnNumber !in 0 until numCols) {
+                println("Invalid column specified")
+                continue@inputcn
+            }
+            columnNumber = inputColumnNumber
+            break
+        }
+
+        return Move(moveType, columnNumber)
     }
 }
