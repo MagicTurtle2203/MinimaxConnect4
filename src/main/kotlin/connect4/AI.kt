23b258b83774f2cac311f3627ae8a7396c6fa7a8
Rafferty Chen <raffertychen@gmail.com>
2020-07-23 21:46:20 -0700

    Rewrite AIAgent

--- src/main/kotlin/connect4/AI.kt
+++ src/main/kotlin/connect4/AI.kt
@@ -1,5 +1,6 @@
 package connect4
 
-abstract class AI(val numCols: Int, val numRows: Int, val lengthToWin: Int, val popOut: Boolean) {
+abstract class AI(private val numCols: Int, private val numRows: Int,
+                  private val lengthToWin: Int, private val popOut: Boolean) {
     abstract fun getMove(boardState: List<List<Char>>): Move
 }
--- src/main/kotlin/connect4/GameRunner.kt
+++ src/main/kotlin/connect4/GameRunner.kt
@@ -11,15 +11,15 @@ class GameRunner(mode: Modes, boardCols: Int, boardRows: Int, lengthToWin: Int =
         when (mode) {
             Modes.DEFAULT -> {
                 player1 = PlayerAgent(boardCols, boardRows, lengthToWin, popOut)
-                player2 = AIAgent(boardCols, boardRows, lengthToWin, popOut, Players.Y, 2)
+                player2 = AIAgent(boardCols, boardRows, lengthToWin, popOut, Players.Y)
             }
             Modes.PVP -> {
                 player1 = PlayerAgent(boardCols, boardRows, lengthToWin, popOut)
                 player2 = PlayerAgent(boardCols, boardRows, lengthToWin, popOut)
             }
             Modes.AIONLY -> {
-                player1 = AIAgent(boardCols, boardRows, lengthToWin, popOut, Players.X, 4)
-                player2 = AIAgent(boardCols, boardRows, lengthToWin, popOut, Players.Y, 2)
+                player1 = AIAgent(boardCols, boardRows, lengthToWin, popOut, Players.X)
+                player2 = AIAgent(boardCols, boardRows, lengthToWin, popOut, Players.Y)
             }
         }
     }
